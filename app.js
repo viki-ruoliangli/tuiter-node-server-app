f271fafe8f15423c2c11ce78d22b680dabee72fb
viki-ruoliangli <liruolianglrllrl@gmail.com>
2023-04-01 20:50:22 -0700

    first commit

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
-import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    ||'mongodb://127.0.0.1:27017/tuiter';
-mongoose.connect(CONNECTION_STRING);
+
 const app = express();
-app.use(cors())
 app.use(express.json());
+app.use(cors())
+
 TuitsController(app);
 HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
-//
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,42 +1,38 @@
-import * as tuitsDao from './tuits-dao.js'
+import posts from "./tuits.js";
+let tuits = posts;
+
 
 const createTuit = async (req, res) => {
   const newTuit = req.body;
+  newTuit._id = (new Date()).getTime()+'';
   newTuit.likes = 0;
   newTuit.liked = false;
-  const insertedTuit = await tuitsDao.createTuit(newTuit);
-  res.json(insertedTuit);
+  tuits.push(newTuit);
+  res.json(newTuit);
 }
 
 
 const findTuits = async (req, res) => {
-  try {
-    const tuits = await tuitsDao.findTuits();
-    res.json(tuits);
-  } catch (err) {
-    res.sendStatus(503);
-  }
+  res.json(tuits);
 }
 
 
 const updateTuit = async (req, res) => {
-  const tuitdIdToUpdate = req.params.tid;
   const updates = req.body;
-
-  const status = await tuitsDao
-  .updateTuit(tuitdIdToUpdate,
-      updates);
-  res.json(status);
+  const tuitIndex = tuits.findIndex(
+      (t) => t._id === tuitdIdToUpdate)
+  tuits[tuitIndex] =
+      {...tuits[tuitIndex], ...updates};
+  res.sendStatus(200);
 }
 
 
 
 const deleteTuit = async(req, res) => {
   const tuitdIdToDelete = req.params.tid;
-  const status = await tuitsDao
-  .deleteTuit(tuitdIdToDelete);
-
-  res.json(status);
+  tuits = tuits.filter((t) =>
+      t._id !== tuitdIdToDelete);
+  res.sendStatus(200);
 }
 
 
