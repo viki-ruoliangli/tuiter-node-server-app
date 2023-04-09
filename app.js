98542ff4f0f56245c5aed410903a5d75859051b4
viki-ruoliangli <liruolianglrllrl@gmail.com>
2023-04-08 23:01:58 -0700

    a9 first commit

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 
+import mongoose from "mongoose";
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//     ||'mongodb://127.0.0.1:27017/tuiter';
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    ||'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(express.json());
 app.use(cors())
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,39 +1,42 @@
-import posts from "./tuits.js";
-let tuits = posts;
-
+import * as tuitsDao from './tuits-dao.js'
 
 const createTuit = async (req, res) => {
   const newTuit = req.body;
-  newTuit._id = (new Date()).getTime()+'';
   newTuit.likes = 0;
   newTuit.liked = false;
-  tuits.push(newTuit);
-  res.json(newTuit);
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
+  res.json(insertedTuit);
 }
 
 
 const findTuits = async (req, res) => {
-  res.json(tuits);
+  try {
+    const tuits = await tuitsDao.findTuits();
+    res.json(tuits);
+  } catch (err) {
+    res.sendStatus(503);
+  }
 }
 
 
 const updateTuit = async (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
   const updates = req.body;
-  const tuitIndex = tuits.findIndex(
-      (t) => t._id === tuitdIdToUpdate)
-  tuits[tuitIndex] =
-      {...tuits[tuitIndex], ...updates};
-  res.sendStatus(200);
+
+  const status = await tuitsDao
+      .updateTuit(tuitdIdToUpdate,
+          updates);
+  res.json(status);
 }
 
 
 
 const deleteTuit = async(req, res) => {
   const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter((t) =>
-      t._id !== tuitdIdToDelete);
-  res.sendStatus(200);
+  const status = await tuitsDao
+      .deleteTuit(tuitdIdToDelete);
+
+  res.json(status);
 }
 
 
