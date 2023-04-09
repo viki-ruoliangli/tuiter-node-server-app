fda895a2578762f67d26ae1ff34d0150452c7959
viki-ruoliangli <liruolianglrllrl@gmail.com>
2023-04-09 02:32:46 -0700

    a9 commit 3

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 
 import mongoose from "mongoose";
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-//     ||'mongodb://127.0.0.1:27017/tuiter';
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     ||'mongodb://127.0.0.1:27017/tuiter';
+
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
@@ -22,4 +21,5 @@ app.use(cors())
 TuitsController(app);
 HelloController(app);
 UserController(app);
+
 app.listen(process.env.PORT || 4000);
